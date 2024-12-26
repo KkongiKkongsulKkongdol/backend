31ecbfd31810ded40d328d4515965e858f243f36
masiljangajji <xmfpdlsj0508@gmail.com>
2024-12-26 14:01:44 +0900

    init: 초기 프로젝트 설정

---
+++ src/test/java/com/teach/me/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teach.me.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
